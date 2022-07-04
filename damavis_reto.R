3fb2157fcd449adbb09e1146b32486eef8fc2674
Enrique <enriquemorales.re.ds@gmail.com>
2022-07-04 21:45:03 +0200

    Improvement of the readability of functions
    Added more comments, changed some variables names on allowedCells and
    added more test.
    
    *PROBLEMS*
    
    The results of the two latest tests doesn't match with the results
    that have been given in the documentation of this challenge.
    
    I think that is a problem related with the indexes of the board and snake.
    The last test "test5", is a modification of Test 2, where I added one column
    to the board, that modification allowed to achieve the desired result.

--- damavis_reto.R
+++ damavis_reto.R
@@ -22,20 +22,24 @@ allowedCells = function(snake, board)
   for(i in c(-1,1))
   {
     # row movement
-    if(i + snake[[1]][1] >= 0 && i + snake[[1]][1] <= board[1])
+    row_move = i + snake[[1]][1]
+    if(row_move >= 0 && row_move < board[1])
     {
-      if(list(c(i + snake[[1]][1], snake[[1]][2])) %in% snake == FALSE)
+      # check if there is self-intersection
+      if(list(c(row_move, snake[[1]][2])) %in% snake == FALSE)
       {
-        possible_cells = append(possible_cells, list(c(i + snake[[1]][1], snake[[1]][2])))
+        possible_cells = append(possible_cells, list(c(row_move, snake[[1]][2])))
       }
     }
     
     # column movement
-    if(i + snake[[1]][2] >= 0 && i + snake[[1]][2] <= board[2])
+    col_move = i + snake[[1]][2] 
+    if(col_move >= 0 && col_move < board[2])
     {
-      if(list(c(snake[[1]][1], i + snake[[1]][2])) %in% snake == FALSE)
+      # check if there is self-intersection
+      if(list(c(snake[[1]][1], col_move)) %in% snake == FALSE)
       {
-        possible_cells = append(possible_cells, list(c(snake[[1]][1], i + snake[[1]][2])))
+        possible_cells = append(possible_cells, list(c(snake[[1]][1], col_move)))
       }
     }
   }
@@ -43,6 +47,7 @@ allowedCells = function(snake, board)
   return(possible_cells)
 }
 
+# using a recursive-backtracking approach to explore all possible solutions 
 numberOfAvailableDifferentPaths = function(board, snake, depth)
 {
   result = 0
@@ -53,12 +58,10 @@ numberOfAvailableDifferentPaths = function(board, snake, depth)
   }
   else
   {
-    # print(snake)
-    allowed = allowedCells(snake, board)
-    for(c in allowed)
+    allowed_cells = allowedCells(snake, board)
+    for(c in allowed_cells)
     {
-      # print(allowedCells(snake, board))
-      result = result + numberOfAvailableDifferentPaths(board, updateSnake(snake, c), depth-1) 
+      result = (result + numberOfAvailableDifferentPaths(board, updateSnake(snake, c), depth-1))%%(1000000007)
     }
   }
   
@@ -66,17 +69,27 @@ numberOfAvailableDifferentPaths = function(board, snake, depth)
 }
 
 #### TEST 1
-board = c(4,3) # columns x rows
-snake = list(c(2,2),c(3,2), c(3,1), c(3,0), c(2,0), c(1,0), c(0,0))
-numberOfAvailableDifferentPaths(board, snake, 3)
+board = c(4,3)
+snake = list( c(2,2), c(3,2), c(3,1), c(3,0), c(2,0), c(1,0), c(0,0) )
+test1 = numberOfAvailableDifferentPaths(board, snake, 3)
 
 #### TEST 2
-board=c(2,3)
-snake= list( c(0,2), c(0,1), c(0,0), c(1,0), c(1,1), c(1,2))
-numberOfAvailableDifferentPaths(board, snake, 10)
+board = c(2,3)
+snake = list( c(0,2), c(0,1), c(0,0), c(1,0), c(1,1), c(1,2) )
+test2 = numberOfAvailableDifferentPaths(board, snake, 10)
 
 #### TEST 3
-board=c(10,10)
-snake= list( c(5,5), c(5,4), c(4,4), c(4,5) )
-numberOfAvailableDifferentPaths(board, snake, 4)
+board = c(10,10)
+snake = list( c(5,5), c(5,4), c(4,4), c(4,5) )
+test3 = numberOfAvailableDifferentPaths(board, snake, 4)
 
+print(c(test1, test2, test3))
+
+#### TEST ADICIONALES
+board = c(2,6)
+snake = list( c(0,2), c(0,1), c(0,0) ) 
+test4 = numberOfAvailableDifferentPaths(board, snake, 2)
+
+board = c(2,4)
+snake = list( c(0,2), c(0,1), c(0,0), c(1,0), c(1,1), c(1,2) ) 
+test5 = numberOfAvailableDifferentPaths(board, snake, 10)
